bade5d86a218689092bba9d869040517df07821e
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-09 19:26:23 +0100

    [js_iniziali]

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,35 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function iniziali(nomi){
+
+    // variabile vuota flag
+
+    var storia = '';
+
+    // ciclo
+
+    for( let i = 0 ; i < nomi.length ; i++ ){
+
+        // salvo nella variabile i singoli nomi della lista 
+        
+        var cose = nomi[i];
+         
+        // prima lettera dei nomi
+
+        var iniziale = cose.charAt(0);
+
+        // inserisco nella variabile 
+
+        storia = `${storia} "${iniziale}",`;
+        
+    }
+
+    return storia;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
